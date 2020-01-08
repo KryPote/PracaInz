ba4554b321a856440f9b082d65a52d60308ffa67
Krystian Potępa <krystian.potepa@gmail.com>
2020-01-08 01:16:50 +0100

    Groundcheck and animation fix

---
+++ Memory 404/Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+
+    private Player player;
+
+
+    private void Start()
+    {
+        player = gameObject.GetComponentInParent<Player>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        player.grounded = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        player.grounded = false;
+    }
+}
---
+++ Memory 404/Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float maxspeed = 3;
+    public float speed = 50f;
+    public float jumpPower = 150f;
+
+    public bool grounded;
+
+    private Rigidbody2D rb2d;
+    private Animator anim;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
+
+    }
+
+    private void Update()
+    {
+        anim.SetBool("Grounded",grounded);
+        anim.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+
+        if(Input.GetAxis("Horizontal") < -0.1f)
+        {
+            transform.localScale = new Vector3(-2, 2, 2);
+        }
+        if (Input.GetAxis("Horizontal") > 0.1f)
+        {
+            transform.localScale = new Vector3(2, 2, 2);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float inputH = Input.GetAxis("Horizontal");
+
+        rb2d.AddForce((Vector2.right * speed) * inputH);
+
+        if(rb2d.velocity.x > maxspeed) //ogarniczenie prędkości w prawo
+        {
+
+            rb2d.velocity = new Vector2(maxspeed, rb2d.velocity.y);
+        }
+        if(rb2d.velocity.x < -maxspeed) //ogarniczenie prędkości w lewo
+        {
+
+            rb2d.velocity = new Vector2(-maxspeed, rb2d.velocity.y);
+        }
+
+    }
+}
