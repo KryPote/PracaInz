33b18c30213eadf07b45993130d9ded90ca80dec
Krystian Potępa <krystian.potepa@gmail.com>
2020-01-14 13:26:36 +0100

    Fixed camera

---
+++ Memory 404/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Vector2 velocity;
+    public float smoothTimeY;
+    public float smoothTimeX;
+
+    public GameObject player;
+
+    void Start()
+    {
+
+    }
+
+
+    void FixedUpdate()
+    {
+        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+        float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
+
+        transform.position = new Vector3(posX,posY, transform.position.z);
+    }
+}
