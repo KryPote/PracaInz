1bd70c61cece2b0ba22f8938fc7e69d71353ceaf
Krystian Potępa <krystian.potepa@gmail.com>
2020-01-15 02:58:45 +0100

    Added PAUSE menu

--- Memory 404/Assets/Scripts/CameraFollow.cs
+++ Memory 404/Assets/Scripts/CameraFollow.cs
@@ -19,7 +19,7 @@ public class CameraFollow : MonoBehaviour
     void FixedUpdate()
     {
         float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
-        float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
+        float posY = 0.25f + Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
 
         transform.position = new Vector3(posX,posY, transform.position.z);
     }
